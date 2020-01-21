7cc36b6e2426ee588936c0b291ef9fdb62750a0e
markligeti <ligetimark.e@gmail.com>
2020-01-21 11:47:33 +0100

    BaseTest class configured to run tests on Selenium Grid

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,29 +1,31 @@
 import POM.DashboardPage;
 import POM.LoginPage;
-import Utils.Util;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 
 @TestInstance(Lifecycle.PER_CLASS)
 public class BaseTest {
     WebDriver driver;
+    String nodeURL;
 
     // POM PAGES
     LoginPage loginPage;
     DashboardPage dashboardPage;
-
     // CREDENTIALS
     String username;
     String password;
-
     // TEST DATA
     String baseUrl;
     List<String> projects;
@@ -33,17 +35,23 @@ public class BaseTest {
     void setupTestEnvironment() {
         this.username = System.getenv("JIRA_USERNAME");
         this.password = System.getenv("JIRA_PASSWORD");
-
+        this.nodeURL = "https://selenium:" + password + "@seleniumhub.codecool.codecanvas.hu/wd/hub";
         this.baseUrl = "https://jira.codecool.codecanvas.hu/";
+
         projects = Arrays.asList("TOUCAN", "JETI", "COALA");
         issueTypes = Arrays.asList("Story", "Task", "Bug");
     }
 
     @BeforeEach
-    void setDriver() {
-        // TODO: Drivers util class, getChromeDriver method + other browsers
-        System.setProperty("webdriver.chrome.driver", Util.getDriverPath("chromedriver"));
-        this.driver = new ChromeDriver();
+    void setDriver() throws MalformedURLException {
+//        // TODO: Drivers util class, getChromeDriver method + other browsers
+//        System.setProperty("webdriver.chrome.driver", Util.getDriverPath("chromedriver"));
+//        this.driver = new ChromeDriver();
+
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        capabilities.setBrowserName("firefox");
+        capabilities.setPlatform(Platform.LINUX);
+        driver = new RemoteWebDriver(new URL(nodeURL), capabilities);
         driver.manage().window().maximize();
 
         loginPage = new LoginPage(driver);
